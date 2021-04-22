1ed7f3f439f72d4554228cc710b547ff513d54d6
Roman Grinyov <w3lifer@gmail.com>
2021-04-22 08:56:13 +0300

    fixed bug with storage

--- js/options.js
+++ js/options.js
@@ -17,9 +17,7 @@ window.addEventListener('DOMContentLoaded', function () {
   textareaHeightElement.addEventListener('input', function (e) {
     chrome.storage.sync.get('textarea', function (storage) {
       let data = {height: e.target.value};
-      if (storage.textarea.width) {
-        data.width = storage.textarea.width;
-      }
+      data.width = (storage.textarea && storage.textarea.width) || TEXTAREA.width;
       chrome.storage.sync.set({textarea: data});
     });
   });
@@ -27,9 +25,7 @@ window.addEventListener('DOMContentLoaded', function () {
   textareaWidthElement.addEventListener('input', function (e) {
     chrome.storage.sync.get('textarea', function (storage) {
       let data = {width: e.target.value};
-      if (storage.textarea.height) {
-        data.height = storage.textarea.height;
-      }
+      data.height = (storage.textarea && storage.textarea.height) || TEXTAREA.height;
       chrome.storage.sync.set({textarea: data});
     });
   });
